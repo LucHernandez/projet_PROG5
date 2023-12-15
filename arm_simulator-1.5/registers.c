2f9ac089674b559e44dcfad7b7a55b0d820d1561
Mateo <mateodacunha42@gmail.com>
2023-12-15 11:45:06 +0100

    ajout des fonctions read/write

--- arm_simulator-1.5/registers.c
+++ arm_simulator-1.5/registers.c
@@ -1,50 +1,50 @@
 /*
-Armator - simulateur de jeu d'instruction ARMv5T à but pédagogique
+Armator - simulateur de jeu d'instruction ARMv5T ï¿½ but pï¿½dagogique
 Copyright (C) 2011 Guillaume Huard
 Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les
-termes de la Licence Publique Générale GNU publiée par la Free Software
-Foundation (version 2 ou bien toute autre version ultérieure choisie par vous).
+termes de la Licence Publique Gï¿½nï¿½rale GNU publiï¿½e par la Free Software
+Foundation (version 2 ou bien toute autre version ultï¿½rieure choisie par vous).
 
-Ce programme est distribué car potentiellement utile, mais SANS AUCUNE
+Ce programme est distribuï¿½ car potentiellement utile, mais SANS AUCUNE
 GARANTIE, ni explicite ni implicite, y compris les garanties de
-commercialisation ou d'adaptation dans un but spécifique. Reportez-vous à la
-Licence Publique Générale GNU pour plus de détails.
+commercialisation ou d'adaptation dans un but spï¿½cifique. Reportez-vous ï¿½ la
+Licence Publique Gï¿½nï¿½rale GNU pour plus de dï¿½tails.
 
-Vous devez avoir reçu une copie de la Licence Publique Générale GNU en même
-temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software
+Vous devez avoir reï¿½u une copie de la Licence Publique Gï¿½nï¿½rale GNU en mï¿½me
+temps que ce programme ; si ce n'est pas le cas, ï¿½crivez ï¿½ la Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
-États-Unis.
+ï¿½tats-Unis.
 
 Contact: Guillaume.Huard@imag.fr
-	 Bâtiment IMAG
+	 Bï¿½timent IMAG
 	 700 avenue centrale, domaine universitaire
-	 38401 Saint Martin d'Hères
+	 38401 Saint Martin d'Hï¿½res
 */
 #include "registers.h"
 #include "arm_constants.h"
 #include <stdlib.h>
 
 struct registers_data {
-    /* À compléter... */
+    /* ï¿½ complï¿½ter... */
 };
 
 registers registers_create() {
     registers r = NULL;
-    /* À compléter... */
+    /* ï¿½ complï¿½ter... */
     return r;
 }
 
 void registers_destroy(registers r) {
-    /* À compléter... */
+    /* ï¿½ complï¿½ter... */
 }
 
 uint8_t registers_get_mode(registers r) {
-    /* À compléter... */
+    /* ï¿½ complï¿½ter... */
     return SVC;
 }
 
 static int registers_mode_has_spsr(registers r, uint8_t mode) {
-    /* À compléter... */
+    /* ï¿½ complï¿½ter... */
     return 1;
 }
 
@@ -53,36 +53,94 @@ int registers_current_mode_has_spsr(registers r) {
 }
 
 int registers_in_a_privileged_mode(registers r) {
-    /* À compléter... */
+    /* ï¿½ complï¿½ter... */
     return 0;
+}
+
+int get_mode_ligne (uint8_t mode){
+    //ont recupere l'index de la ligne du tableau qui corespond au mode
+    switch (mode) 
+    {
+        case USR :
+            return 0;
+            break;
+        case SYS :
+            return 1;
+            break;
+        case SVC :
+            return 2;
+            break;
+        case ABT :
+            return 3;
+            break;
+        case UND :
+            return 4;
+            break;
+        case IRQ :
+            return 5;
+            break;
+        case FIQ :
+            return 6;
+            break;
+        default :
+            exit(2);
+            break;
+    };
 }
 
 uint32_t registers_read(registers r, uint8_t reg, uint8_t mode) {
     uint32_t value = 0;
-    /* À compléter... */
+    int ligne=get_mode_ligne(mode);  //ligne corespondant au mode
+
+    //ont recupere la valeur du registre
+    value=r->correspondance_modes[ligne][reg];
+
     return value;
 }
 
 uint32_t registers_read_cpsr(registers r) {
     uint32_t value = 0;
-    /* À compléter... */
+
+    //ont, renvois le registre CPSR qui est le meme pour chaque mode
+    value=r->correspondance_modes[0][16];
+
     return value;
 }
 
 uint32_t registers_read_spsr(registers r, uint8_t mode) {
     uint32_t value = 0;
-    /* À compléter... */
+
+    // test si le mode a accee au registre SPSR
+    if (registers_mode_has_spsr(r,mode)){
+        value=correspondance_modes[get_mode_ligne(mode)][17];
+    }
+    else{
+        //exit si le mode ne le permer pas
+        exit(2);
+    }
+
     return value;
 }
 
 void registers_write(registers r, uint8_t reg, uint8_t mode, uint32_t value) {
-    /* À compléter... */
+    //ont recupere la valeur du registre
+    int ligne=get_mode_ligne(mode); //ligne corespondant au mode
+
+    //ont change la valeur du registre
+    *(r->correspondance_modes[ligne][reg])=value;
 }
 
 void registers_write_cpsr(registers r, uint32_t value) {
-    /* À compléter... */
+    *(r->correspondance_modes[0][16])=value;
 }
 
 void registers_write_spsr(registers r, uint8_t mode, uint32_t value) {
-    /* À compléter... */
+    // test si le mode a accee au registre SPSR
+    if (registers_mode_has_spsr(r,mode)){
+        *(correspondance_modes[get_mode_ligne(mode)][17])=value;
+    }
+    else{
+        //exit si le mode ne le permer pas
+        exit(2);
+    }
 }
