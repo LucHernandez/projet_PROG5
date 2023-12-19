adb743e728908b2288b791e6c4ed00e4dfdc3790
JAX <jojo38260@hotmail.fr>
2023-12-19 12:26:04 +0100

    gestion des 3 imediate de address_mode

--- arm_simulator-1.5/arm_load_store.c
+++ arm_simulator-1.5/arm_load_store.c
@@ -1,24 +1,24 @@
 /*
-Armator - simulateur de jeu d'instruction ARMv5T à but pédagogique
+Armator - simulateur de jeu d'instruction ARMv5T ï¿½ but pï¿½dagogique
 Copyright (C) 2011 Guillaume Huard
 Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les
-termes de la Licence Publique Générale GNU publiée par la Free Software
-Foundation (version 2 ou bien toute autre version ultérieure choisie par vous).
+termes de la Licence Publique Gï¿½nï¿½rale GNU publiï¿½e par la Free Software
+Foundation (version 2 ou bien toute autre version ultï¿½rieure choisie par vous).
 
-Ce programme est distribué car potentiellement utile, mais SANS AUCUNE
+Ce programme est distribuï¿½ car potentiellement utile, mais SANS AUCUNE
 GARANTIE, ni explicite ni implicite, y compris les garanties de
-commercialisation ou d'adaptation dans un but spécifique. Reportez-vous à la
-Licence Publique Générale GNU pour plus de détails.
+commercialisation ou d'adaptation dans un but spï¿½cifique. Reportez-vous ï¿½ la
+Licence Publique Gï¿½nï¿½rale GNU pour plus de dï¿½tails.
 
-Vous devez avoir reçu une copie de la Licence Publique Générale GNU en même
-temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software
+Vous devez avoir reï¿½u une copie de la Licence Publique Gï¿½nï¿½rale GNU en mï¿½me
+temps que ce programme ; si ce n'est pas le cas, ï¿½crivez ï¿½ la Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
-États-Unis.
+ï¿½tats-Unis.
 
 Contact: Guillaume.Huard@imag.fr
-	 Bâtiment IMAG
+	 Bï¿½timent IMAG
 	 700 avenue centrale, domaine universitaire
-	 38401 Saint Martin d'Hères
+	 38401 Saint Martin d'Hï¿½res
 */
 #include "arm_load_store.h"
 #include "arm_exception.h"
@@ -37,4 +37,59 @@ int arm_load_store_multiple(arm_core p, uint32_t ins) {
 int arm_coprocessor_load_store(arm_core p, uint32_t ins) {
     /* Not implemented */
     return UNDEFINED_INSTRUCTION;
+}
+
+int verif_addr_mode(arm_core p,uint32_t ins){
+    int Rn = get_bits(ins,19,16);
+    int result = 0;
+    if(get_bit(ins,25)==0){ //I==0
+        int offset = get_bits(ins,11,0);
+        if(get_bit(ins,24)==1){ // P==1
+            if(get_bit(ins,21)==1){ // W==1
+                if(get_bit(ins,23)==1){ // U==1
+                    result = Rn + offset;
+                    arm_write_register(p,(uint8_t)Rn,result);
+                    return result;
+                }
+                else{ // U==0
+                    result = Rn - offset;
+                    arm_write_register(p,(uint8_t)Rn,result);
+                    return result;
+                }
+            }
+            else{ // W==0
+                if(get_bit(ins,23)==1){ // U==1
+                    result = Rn + offset;
+                    return result;
+                }
+                else{ // U==0
+                    result = Rn - offset;
+                    return result;
+                }  
+            }
+        }
+        else{ //P==0
+            if(get_bit(ins,21)==1){ // W==1
+                if(get_bit(ins,23)==1){ // U==1
+                    return UNDEFINED_INSTRUCTION;
+                }
+                else{ // U==0
+                    return UNDEFINED_INSTRUCTION;
+                }
+            }
+            else{ // W==0
+                if(get_bit(ins,23)==1){ // U==1
+                    result = Rn + offset;
+                    arm_write_register(p,(uint8_t)Rn,result);
+                    return Rn;
+                }
+                else{ // U==0
+                    result = Rn - offset;
+                    arm_write_register(p,(uint8_t)Rn,result);
+                    return Rn;
+                }
+            }
+
+        }
+    }
 }
