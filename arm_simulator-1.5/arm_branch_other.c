18998d47ce67f4216e5d2e89c616e3983dd8430e
sïan <siansigsavourat@gmail.com>
2023-12-27 02:44:57 -0100

    implementation de l'instruction MRS , et de l'instruction B/BL avec un soucis pour l'accees a PC

--- arm_simulator-1.5/arm_branch_other.c
+++ arm_simulator-1.5/arm_branch_other.c
@@ -1,24 +1,24 @@
 /*
-Armator - simulateur de jeu d'instruction ARMv5T à but pédagogique
+Armator - simulateur de jeu d'instruction ARMv5T ï¿½ but pï¿½dagogique
 Copyright (C) 2011 Guillaume Huard
 Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les
-termes de la Licence Publique Générale GNU publiée par la Free Software
-Foundation (version 2 ou bien toute autre version ultérieure choisie par vous).
+termes de la Licence Publique Gï¿½nï¿½rale GNU publiï¿½e par la Free Software
+Foundation (version 2 ou bien toute autre version ultï¿½rieure choisie par vous).
 
-Ce programme est distribué car potentiellement utile, mais SANS AUCUNE
+Ce programme est distribuï¿½ car potentiellement utile, mais SANS AUCUNE
 GARANTIE, ni explicite ni implicite, y compris les garanties de
-commercialisation ou d'adaptation dans un but spécifique. Reportez-vous à la
-Licence Publique Générale GNU pour plus de détails.
+commercialisation ou d'adaptation dans un but spï¿½cifique. Reportez-vous ï¿½ la
+Licence Publique Gï¿½nï¿½rale GNU pour plus de dï¿½tails.
 
-Vous devez avoir reçu une copie de la Licence Publique Générale GNU en même
-temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software
+Vous devez avoir reï¿½u une copie de la Licence Publique Gï¿½nï¿½rale GNU en mï¿½me
+temps que ce programme ; si ce n'est pas le cas, ï¿½crivez ï¿½ la Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
-États-Unis.
+ï¿½tats-Unis.
 
 Contact: Guillaume.Huard@imag.fr
-	 Bâtiment IMAG
+	 Bï¿½timent IMAG
 	 700 avenue centrale, domaine universitaire
-	 38401 Saint Martin d'Hères
+	 38401 Saint Martin d'Hï¿½res
 */
 #include "arm_branch_other.h"
 #include "arm_constants.h"
@@ -27,9 +27,70 @@ Contact: Guillaume.Huard@imag.fr
 #include <stdlib.h>
 
 
-int arm_branch(arm_core p, uint32_t ins) {
-    return UNDEFINED_INSTRUCTION;
+
+uint32_t decod_adr (uint32_t adr){
+    //ont etant le 23 eme bit 
+    //si le bit est 1 alors ont set tout les bits jusqua 30 a 1
+    //si non rien besoin de faire car ils sont deja egal a 0
+    if(get_bit(adr,23)==1){
+        set_bit(adr,24);
+        set_bit(adr,25);
+        set_bit(adr,26);
+        set_bit(adr,27);
+        set_bit(adr,28);
+        set_bit(adr,29);
+    }
+
+    //ont shifte de 2 bit pour obtenire une adresse en 32 bits
+    adr=adr<<2;
+
+    //renvois de l'adresse decoder
+    return adr;
 }
+
+
+int arm_branch_other_b_bl(arm_core p, uint32_t ins){
+
+    //test si le bit L (bit 24) est a un 
+    if (get_bit(ins,24)==1){
+        //stock l'adresse de PC dans le registre 14 aussi nomee LR
+        arm_write_register(p,14,arm_read_register(p,PC));
+    }
+
+    //decode le saut pour la suite des instruction a executer
+    uint32_t adr=decod_adr(get_bits(ins,23,0));
+
+    arm_write_register(p,PC,arm_read_register(p,PC)+adr);
+
+    return 0;
+}
+
+int arm_branche_other_mrs (arm_core p, uint32_t ins){
+    //ont recupere le numero du registre destination
+    uint32_t numero_reg=get_bits(ins,15,12);
+
+    //ont verifie que le registre destination n'est pas R15 car il est UNPREDICTABLE
+    if(numero_reg==15){
+        exit(1);
+    }
+
+    //si non ont continue le deroulement normal
+    //ont regarde le bit 22 qui nous dit si ont veut copier CPSR ou SPSR
+    if(get_bit(ins,22)==1){
+        //test si le mode actuel a bien un registre spcr
+        if(registers_current_mode_has_spsr(p)){
+            arm_write_register(p,numero_reg,arm_read_spsr(p));
+        }
+        else{
+            exit(2);
+        }
+    }
+    else{
+        arm_write_register(p,numero_reg,arm_read_cpsr(p));
+    }
+    return 0;
+}
+
 
 int arm_coprocessor_others_swi(arm_core p, uint32_t ins) {
     if (get_bit(ins, 24)) {
